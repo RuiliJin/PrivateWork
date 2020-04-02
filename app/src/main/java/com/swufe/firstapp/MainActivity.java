bc9d70079059e0bcb43ca29bf37a350237153ef3
RuiliJin <2372549657@qq.com>
2020-04-02 22:56:56 +0800

    translate

--- app/src/main/java/com/swufe/firstapp/MainActivity.java
+++ app/src/main/java/com/swufe/firstapp/MainActivity.java
@@ -8,17 +8,38 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView out;
+    EditText inp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//add something.......
+
+        out = (TextView)findViewById(R.id.showText);
+        inp = (EditText)findViewById(R.id.inpText);
+
+        Button btn = (Button)findViewById(R.id.btn1);
+        btn.setOnClickListener(this);
+
+
     }
 
+    @Override
+    public void onClick(View v) {
+        Log.i("click","onclick");
+        EditText inp = (EditText)findViewById(R.id.inpText);
+        double in = Double.parseDouble(inp.getText().toString());
+        inp.setText(in+"摄氏度");
+        double ou = in*33.8;
+        out.setText("结果是"+ou+"华氏度");
+    }
 }
