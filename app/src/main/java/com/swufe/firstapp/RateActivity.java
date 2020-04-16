de96e05bdf3a7e8fe61e778089f81b4ca16339fc
RuiliJin <2372549657@qq.com>
2020-04-16 15:24:22 +0800

    rate

--- app/src/main/java/com/swufe/firstapp/RateActivity.java
+++ app/src/main/java/com/swufe/firstapp/RateActivity.java
@@ -1,45 +1,115 @@
 package com.swufe.firstapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class RateActivity extends AppCompatActivity {
+    private final String TAG = "Rate";
+    private float dollarRate = 0.1f;
+    private float euroRate = 0.2f;
+    private float wonRate = 0.3f;
+
     TextView show;
     EditText rmb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rate);
-        show = (TextView)findViewById(R.id.rmb);
-        rmb=(EditText)findViewById(R.id.rmb);
+        show = (TextView) findViewById(R.id.rmb);
+        rmb = (EditText) findViewById(R.id.rmb);
     }
-    public void onClick(View btn){
-        float r=0,val=0;
-        String str=rmb.getText().toString();
-        if(str.length()>0){
-            r=Float.parseFloat(str);
-        }else{
-            Toast.makeText(this,"请输入金额",Toast.LENGTH_SHORT).show();
+
+    public void onClick(View btn) {
+        Log.i(TAG, "onClick:");
+        String str = rmb.getText().toString();
+        Log.i(TAG, "onClick: get str=" + str);
+
+        float r = 0;//val=0;
+        if (str.length() > 0) {
+            r = Float.parseFloat(str);
+        } else {//没有输入内容
+            Toast.makeText(this, "请输入金额", Toast.LENGTH_SHORT).show();
         }
-        if(btn.getId()==R.id.btn_dollar){
-            val= r*(1/6.7f);
+        Log.i(TAG, "onClick:r=" + r);
+        //计算
+        /*if(btn.getId()==R.id.btn_dollar){  //常量
+           val= r*(1/6.7f);
         }else if(btn.getId()==R.id.btn_euro){
             val = r*(1/11f);
-        }else{
-            val=r*500;
-        }show.setText(val+"");//show.setText(String.valueOf(val))
+      }else{
+          val=r*500;
+    }show.setText(val+"");//show.setText(String.valueOf(val))
+    }*/
+        if (btn.getId() == R.id.btn_dollar) { //变量
+            show.setText(String.format("%.2f", r * dollarRate));
+        } else if (btn.getId() == R.id.btn_euro) {
+            show.setText(String.format("%.2f", r * euroRate));
+        } else {
+            show.setText(String.format("%.2f", r * wonRate));
+        }
     }
-    public void openOne(View btn){
-        Intent hello = new Intent(this,SecondActivity.class);
+
+    /**
+     * @param btn
+     */
+    public void openOne(View btn) {
+        openConfig();
+    }
+
+    private void openConfig() {
+        Intent config = new Intent(this, ConfigActivity.class);
         //打开一个页面
-       //打开网页 Intent web = new Intent(Intent.ACTION_VIEW,Uri.parse("http..."));
+        //打开网页 Intent web = new Intent(Intent.ACTION_VIEW,Uri.parse("http..."));
         //Intent  phone = new Intent(Intent.ACTION_DIAL,Uri.parse("183888"));
-        startActivity(hello);
+        config.putExtra("dollar_rate_key", dollarRate);
+        config.putExtra("euro_rate_key", euroRate);
+        config.putExtra("won_rate_key", wonRate);
+
+        Log.i(TAG, "openOne:dollar_rate_key=" + dollarRate);
+        Log.i(TAG, "openOne:euro_rate_key=" + euroRate);
+        Log.i(TAG, "openOne:won_rate_key=" + wonRate);
+
+        //startActivity(config);
+        startActivityForResult(config, 1);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.rate,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.menu_set){
+            openConfig();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 1 && resultCode == 2) {
+            Bundle bundle = data.getExtras();
+            dollarRate = bundle.getFloat("key_dollar", 0.1f);
+            euroRate = bundle.getFloat("key_euro", 0.2f);
+            wonRate = bundle.getFloat("key_won", 0.3f);
+            Log.i(TAG, "onActivityResult:dollarRate=" + dollarRate);
+            Log.i(TAG, "onActivityResult:euroRate=" + euroRate);
+            Log.i(TAG, "onActivityResult:wonRate=" + wonRate);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
