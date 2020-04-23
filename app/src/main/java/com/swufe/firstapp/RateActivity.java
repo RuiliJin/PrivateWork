d93cf61f6026a6d485a3c864f5c0952b852816f0
RuiliJin <2372549657@qq.com>
2020-04-23 16:29:31 +0800

    update

--- app/src/main/java/com/swufe/firstapp/RateActivity.java
+++ app/src/main/java/com/swufe/firstapp/RateActivity.java
@@ -18,13 +18,18 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class RateActivity extends AppCompatActivity implements Runnable {
     private final String TAG = "Rate";
@@ -36,16 +41,16 @@ public class RateActivity extends AppCompatActivity implements Runnable {
     EditText rmb;
     Handler handler;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rate);
         show = (TextView) findViewById(R.id.shouOut);
         rmb = (EditText) findViewById(R.id.rmb);
-
         //获取SP里保存的数据
         SharedPreferences sharedPreferences = getSharedPreferences("myrate", MODE_PRIVATE);
-        SharedPreferences sp= PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
         dollarRate = sharedPreferences.getFloat("dollar_rate", 0.0f);
         euroRate = sharedPreferences.getFloat("euro_rate", 0.0f);
         wonRate = sharedPreferences.getFloat("won_rate", 0.0f);
@@ -53,22 +58,54 @@ public class RateActivity extends AppCompatActivity implements Runnable {
         Log.i(TAG, "onCreate:sp euroRate=" + euroRate);
         Log.i(TAG, "onCreate:sp wonRate=" + wonRate);
 
-        //开启子线程
-        Thread t=new Thread(this);
-        t.start();
+        //获取当前系统时间
+        String updateDate = sharedPreferences.getString("update_date", " ");
+        Date today = Calendar.getInstance().getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        final String todayStr = sdf.format(today);
+        Log.i(TAG, "onCreate:todayStr=" + todayStr);
+        //判断时间
+        if (!todayStr.equals(updateDate)) {
+            Log.i(TAG, "onCreate:需要更新");
+            //开启子线程
+            Thread t = new Thread(this);
+            t.start();
+        } else {
+            Log.i(TAG, "onCreate:不需更新");
+        }
 
-        handler=new Handler() {
+
+        handler = new Handler() {
             @Override
             public void handleMessage(@NonNull Message msg) {
-                if(msg.what==5){
-                    String str= (String) msg.obj;
-                    Log.i(TAG,"handleMessage:getMessage msg="+str);
-                    show.setText(str);
+                if (msg.what == 5) {
+                    Bundle bdl = (Bundle) msg.obj;
+                    dollarRate = bdl.getFloat("dollar-rate");
+                    euroRate = bdl.getFloat("euro-rate");
+                    wonRate = bdl.getFloat("won-rate");
+                    Log.i(TAG, "handleMessage:dollarRate=" + dollarRate);
+                    Log.i(TAG, "handleMessage:euroRate=" + euroRate);
+                    Log.i(TAG, "handleMessage:wonRate=" + wonRate);
+                    Toast.makeText(RateActivity.this, "汇率已更新", Toast.LENGTH_SHORT).show();
+                    //Log.i(TAG,"handleMessage:getMessage msg="+str);
+                    //show.setText(str);
+
+                    //将新获得汇率写到sp中
+                    SharedPreferences sharedPreferences = getSharedPreferences("myrate", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putFloat("dollar_rate", dollarRate);
+                    editor.putFloat("euro_rate", euroRate);
+                    editor.putFloat("won_rate", wonRate);
+                    editor.putString("update_date", todayStr);
+                    editor.apply();
+                    Log.i(TAG, "onActivityResult: " + (Calendar.getInstance().getTime()).toString() + "更新数据已保存到SP");
+
                 }
                 super.handleMessage(msg);
             }
         };
     }
+
     public void onClick(View btn) {
         Log.i(TAG, "onClick:");
         String str = rmb.getText().toString();
@@ -99,7 +136,6 @@ public class RateActivity extends AppCompatActivity implements Runnable {
         }
     }
 
-
     public void openOne(View btn) {
         openConfig();
     }
@@ -123,13 +159,13 @@ public class RateActivity extends AppCompatActivity implements Runnable {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.rate,menu);
+        getMenuInflater().inflate(R.menu.rate, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId()==R.id.menu_set){
+        if (item.getItemId() == R.id.menu_set) {
             openConfig();
         }
         return super.onOptionsItemSelected(item);
@@ -149,49 +185,142 @@ public class RateActivity extends AppCompatActivity implements Runnable {
             //将新获得汇率写到sp中
             SharedPreferences sharedPreferences = getSharedPreferences("myrate", MODE_PRIVATE);
             SharedPreferences.Editor editor = sharedPreferences.edit();
-            editor.putFloat("dollar_rate",dollarRate);
-            editor.putFloat("euro_rate",euroRate);
-            editor.putFloat("won_rate",wonRate);
+            editor.putFloat("dollar_rate", dollarRate);
+            editor.putFloat("euro_rate", euroRate);
+            editor.putFloat("won_rate", wonRate);
             editor.commit();
-            Log.i(TAG,"onActivityResult: 数据已保存到SP");
+            Log.i(TAG, "onActivityResult: 数据已保存到SP");
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
     public void run() {
-        Log.i(TAG,"run:run....");
-        for(int i=1;i<6;i++){
-            Log.i(TAG,"run:i="+i);
-            try{
-                Thread.sleep(2000);
+        Log.i(TAG, "run:run....");
+        for (int i = 1; i < 6; i++) {
+            Log.i(TAG, "run:i=" + i);
+            try {
+                Thread.sleep(0);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+        //用于保存获取的数据
 
-        //获取msg对象，用于返回主线程
-        Message msg=handler.obtainMessage(5);
-       // msg.what=5;
-        msg.obj="hello from Run()";
-        handler.sendMessage(msg);
 
         //获取网络数据
-        try {
-            URL url = new URL("https://www.usd-cny.com/icbc.htm");
+        /*try {
+            URL url = new URL("https://www.usd-cny.com/bankofchina.htm");
             HttpURLConnection http= (HttpURLConnection) url.openConnection();
             InputStream in=http.getInputStream();
 
             String html = inputSteamString(in);
             Log.i(TAG,"run:html="+html);
+            Document doc=Jsoup.parse(html);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
+        Bundle bundle = new Bundle();
+        bundle = getFromBOC();
+        //获取msg对象，用于返回主线程
+        Message msg = handler.obtainMessage(5);
+        // msg.what=5;
+        //msg.obj="hello from Run()";
+        msg.obj = bundle;
+        handler.sendMessage(msg);
 
 
     }
+
+    private Bundle getFromBOC() {
+        Bundle bundle = new Bundle();
+        Document doc = null;
+        try {
+            doc = Jsoup.connect("https://www.boc.cn/sourcedb/whpj/").get();
+            Log.i(TAG, "run:" + doc.title());
+            Elements tables = doc.getElementsByTag("table");
+           /* int i=1;
+            for(Element table :tables){
+                Log.i(TAG,"run:table["+i+"]="+table);
+                i++;
+
+            }*/
+            Element table = tables.get(0);
+            //Log.i(TAG,"run:table[0]="+table);
+            //获取td中的数据
+            Elements tds = table.getElementsByTag("td");
+            for (int i = 0; i < tds.size(); i += 6) {
+                Element td1 = tds.get(i);
+                Log.i(TAG, "run:text=" + td1.text());
+                Element td2 = tds.get(i + 5);
+                Log.i(TAG, "run:text=" + td2.text());
+                String str1 = td1.text();
+                String val = td2.text();
+                if ("美元".equals(str1)) {
+                    bundle.putFloat("dollar-rate", 100f / Float.parseFloat(val));
+                    Log.i(TAG, "putFloat:dollar-rate=" + 100f / Float.parseFloat(val));
+                } else if ("欧元".equals(str1)) {
+                    bundle.putFloat("euro-rate", 100f / Float.parseFloat(val));
+                } else if ("韩元".equals(str1)) {
+                    bundle.putFloat("won-rate", 100f / Float.parseFloat(val));
+                }
+            }
+            /*for(Element td :tds){
+                Log.i(TAG,"run:td="+td);
+                Log.i(TAG,"run:text="+td.text());
+                Log.i(TAG,"run:html="+td.html());
+            }*/
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return  bundle;
+    }
+    private Bundle getFromUsdCny() {
+        Bundle bundle = new Bundle();
+        Document doc = null;
+        try {
+            doc = Jsoup.connect("https://www.usd-cny.com/bankofchina.htm").get();
+            Log.i(TAG, "run:" + doc.title());
+            Elements tables = doc.getElementsByTag("table");
+           /* int i=1;
+            for(Element table :tables){
+                Log.i(TAG,"run:table["+i+"]="+table);
+                i++;
+
+            }*/
+            Element table = tables.get(0);
+            //Log.i(TAG,"run:table[0]="+table);
+            //获取td中的数据
+            Elements tds = table.getElementsByTag("td");
+            for (int i = 0; i < tds.size(); i += 6) {
+                Element td1 = tds.get(i);
+                Log.i(TAG, "run:text=" + td1.text());
+                Element td2 = tds.get(i + 5);
+                Log.i(TAG, "run:text=" + td2.text());
+                String str1 = td1.text();
+                String val = td2.text();
+                if ("美元".equals(str1)) {
+                    bundle.putFloat("dollar-rate", 100f / Float.parseFloat(val));
+                    Log.i(TAG, "putFloat:dollar-rate=" + 100f / Float.parseFloat(val));
+                } else if ("欧元".equals(str1)) {
+                    bundle.putFloat("euro-rate", 100f / Float.parseFloat(val));
+                } else if ("韩元".equals(str1)) {
+                    bundle.putFloat("won-rate", 100f / Float.parseFloat(val));
+                }
+            }
+            /*for(Element td :tds){
+                Log.i(TAG,"run:td="+td);
+                Log.i(TAG,"run:text="+td.text());
+                Log.i(TAG,"run:html="+td.html());
+            }*/
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return  bundle;
+    }
+
     private String inputSteamString(InputStream inputStream) throws IOException {
         final int bufferSize=1024;
         final char[] buffer=new char[bufferSize];
@@ -204,5 +333,6 @@ public class RateActivity extends AppCompatActivity implements Runnable {
             }out.append(buffer,0,rsz);
         }
         return out.toString();
+        //完成
     }
 }
--- app/src/main/java/com/swufe/firstapp/SecondActivity.java
+++ app/src/main/java/com/swufe/firstapp/SecondActivity.java
@@ -1,21 +1,80 @@
 package com.swufe.firstapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class SecondActivity extends AppCompatActivity {
-    TextView scoreA,scoreB;
+    private  final String TAG="SecondActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Log.i(TAG,"onCreate");
 
-        scoreA = (TextView) findViewById(R.id.scoreA);
-        scoreB = (TextView) findViewById(R.id.scoreB);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        String  scoreA = findViewById(R.id.scoreA).toString();
+        String scoreB =  findViewById(R.id.scoreB).toString();
+        outState.putString("teamA-score",scoreA);
+        outState.putString("teamB-score",scoreB);
+
+        Log.i(TAG,"onSaveInstanceState");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String scoreA=savedInstanceState.getString("teamA-score");
+        String scoreB=savedInstanceState.getString("teamB-score");
+        ((TextView)findViewById(R.id.scoreA)).setText(scoreA);
+        ((TextView)findViewById(R.id.scoreA)).setText(scoreB);
+
+        Log.i(TAG,"onRestoreInstanceState");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"onResume");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG,"onRestart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"onDestroy");
+    }
+
     public void btn_1(View btn){
         if(btn.getId()==R.id.btn_A1){
             showScoreA(1);
@@ -39,15 +98,20 @@ public class SecondActivity extends AppCompatActivity {
         }
     }
     public void btn_r(View btn){
+        TextView scoreA = (TextView) findViewById(R.id.scoreA);
+        TextView scoreB = (TextView) findViewById(R.id.scoreB);
         scoreA.setText("0");
         scoreB.setText("0");
     }
     public void showScoreA (int addA) {
+        TextView scoreA = (TextView) findViewById(R.id.scoreA);
+
         String oldScoreA = (String) scoreA.getText();
         int newScoreA = Integer.parseInt(oldScoreA) + addA;
         scoreA.setText("" + newScoreA);
     }
     public void showScoreB(int addB) {
+        TextView scoreB = (TextView) findViewById(R.id.scoreB);
         String oldScoreB = (String) scoreB.getText();
         int newScoreB = Integer.parseInt(oldScoreB) + addB;
         scoreB.setText("" + newScoreB);
